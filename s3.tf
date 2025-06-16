24e896406376e3c9564ce4f68e3d4a3518113bc8
Chandan Tippari K K <tipparichandan@gmail.com>
2025-06-16 14:43:27 +0530

    Update s3.tf
    commit changes done

--- s3.tf
+++ s3.tf
@@ -2,11 +2,6 @@ resource "aws_s3_bucket" "logs" {
   bucket = var.bucket_name
 }
 
-resource "aws_s3_bucket_acl" "logs_acl" {
-  bucket = aws_s3_bucket.logs.id
-  acl    = "private"
-}
-
 resource "aws_s3_bucket_lifecycle_configuration" "lifecycle" {
   bucket = aws_s3_bucket.logs.id
   rule {
